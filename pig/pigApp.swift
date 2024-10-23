11be3f6d1b921faa9025a9c48a0064874dfc832d
Rohan Patel <rpatel5@bsd@220.org>
2024-10-23 10:07:03 -0500

    Initial Commit

---
+++ pig/pigApp.swift
@@ -0,0 +1,17 @@
+//
+//  pigApp.swift
+//  pig
+//
+//  Created by rohan patel on 10/23/24.
+//
+
+import SwiftUI
+
+@main
+struct pigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pigTests/pigTests.swift
@@ -0,0 +1,17 @@
+//
+//  pigTests.swift
+//  pigTests
+//
+//  Created by rohan patel on 10/23/24.
+//
+
+import Testing
+@testable import pig
+
+struct pigTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
