977ddcbea3ffa84e29814f4117b8e88c815e7f06
Rohan Patel <rpatel5@bsd@220.org>
2024-10-24 10:04:21 -0500

    beteer one

--- pig/ContentView.swift
+++ pig/ContentView.swift
@@ -8,14 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var turnScore = 0
+    @State private var gameScore = 0
+    @State private var randomValue = 0
+    @State private var rotation = 0.0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.gray.opacity(0.7).ignoresSafeArea()
+            VStack {
+                Image("Pig").resizable().frame(width: 150, height: 150)
+                Text ("Pig")
+                Spacer()
+                CustomText(text: "Turn Score: \(turnScore)")
+                CustomText(text: "Game Score: \(gameScore)")
+               Image("pips \(randomValue)")
+                  .resizable()
+                  .frame(width: 150, height: 150)
+                  .rotationEffect(.degrees(rotation))
+                  .rotation3DEffect(.degrees(rotation), axis: (x: 1, y: 1, z: 0))
+                   .padding()
+            }
         }
-        .padding()
+    }
+}
+
+struct CustomText: View {
+    let text: String
+    var body: some View {
+        Text(text).font(Font.custom("Marker Felt", size: 36))
+        
     }
 }
 
